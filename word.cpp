5491bcad96e60e9292018b57da20b102bfc8cc2c
MichaelFar <31226358+MichaelFar@users.noreply.github.com>
2018-12-03 15:26:12 -0800

    Update word.cpp

--- word.cpp
+++ word.cpp
@@ -1,45 +1,48 @@
-#include "word.h"
+// TODO -- needs implementation
+	#include "word.h"
 
-Word::Word(string newWord) {
-  word = newWord;
-  count = 1;
-}
-
-string Word::getWord() {
-  return word;
-}
-
-unsigned int Word::getCount() {
-  return count;
-}
-
-void Word::setWord(string newWord) {
-  string = newWord;
-}
-
-void Word::incrementCount(unsigned int amount) {
-  count += amount;
-}
-
-// operator>
-bool Word::operator>(const Word& otherWord) {
-  // TODO
-}
-
-// operator<
-bool Word::operator<(const Word& otherWord) {
-  // TODO
-}
-
-// operator==
-bool Word::operator==(const Word& otherWord) {
-  // TODO
-}
-
-friend ostream& operator<<(ostream& outs, const Word& w) {
-  // TODO
-}
-
-string Word::uppercase(string s) {
-  // TODO
-}
+	string Word::getWord() 
+	{
+		
+	}
+	unsigned int Word ::getCount() 
+	{
+		
+		
+	}
+	
+	void Word ::setWord(string newWord) 
+	{
+		
+	}
+	void Word ::incrementCount(unsigned int amount) 
+	{
+		
+	}
+	
+	bool Word ::operator> (const Word& otherWord) 
+	{
+		return this -> word > otherWord.word;
+	}
+	
+	bool Word ::operator< (const Word& otherWord) 
+	{
+		return this -> word < otherWord.word;
+	}
+	
+	bool Word ::operator==(const Word& otherWord) 
+	{
+		return this -> word == otherWord.word;
+	}
+	
+	friend ostream& operator<<(ostream& outs, const Word& otherWord) 
+	{
+		
+		outs << otherWord.word;
+		return outs;
+	}
+	
+	string uppercase(string s) 
+	{
+	
+	}
